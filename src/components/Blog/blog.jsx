d30948a04a9750a199c43001d92887e110a56005
sean292002 <88092483+sean292002@users.noreply.github.com>
2023-12-06 16:48:13 -0500

    Update blog.jsx
    Update link

--- src/components/Blog/blog.jsx
+++ src/components/Blog/blog.jsx
@@ -14,12 +14,8 @@ class Blogs extends Component {
     // https://www.toptal.com/developers/feed2json/convert?url=
     // https://api.rss2json.com/v1/api.json?rss_url=
 
-    axios.get(`https://www.toptal.com/developers/feed2json/convert?url=${mediumRssFeed}`, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0',
-      }
-    })
-    .then((response) => {
+    axios.get(`https://www.convertjson.com/cgi-bin/url-to-json.php?callback=loadDataAndRun&url=${mediumRssFeed}`)
+      .then((response) => {
         this.setState({ blogs: response.data.items });
       })
       .catch((error) => {
